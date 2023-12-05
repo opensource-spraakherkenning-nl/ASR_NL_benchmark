2a36c17427cd3c9e2a77de74f78a9272f81fe18c
Dragoș <balandragos5555@gmail.com>
2023-12-05 14:06:41 +0100

    Test removing -m hyp from sclite command in pipeline

--- ASR_NL_benchmark/pipeline.py
+++ ASR_NL_benchmark/pipeline.py
@@ -58,7 +58,7 @@ def run_pipeline(hypfile, reffile, skip_ref_norm, skip_hyp_norm):
     run = os.system(
         f"csrfilt.sh -s -i stm {os.path.join('ASR_NL_benchmark','variations.glm')} < {reffile.normalized_path} > {reffile.variation_path}")
     run = os.system(
-        f"sclite -D -h {hypfile.variation_path} {hypfile.extension} -r {reffile.variation_path} {reffile.extension} -m hyp -O {os.path.join(os.path.sep,'input','results')} -o dtl spk")
+        f"sclite -D -h {hypfile.variation_path} {hypfile.extension} -r {reffile.variation_path} {reffile.extension} -O {os.path.join(os.path.sep,'input','results')} -o dtl spk")
 
 def calculate_wer(df):
     """ Calculates the word error rate and adds the collumn 'product' to the dataframe
